85773fa5f5ca3e2764cd8a173518782c92d08c8e
Michael John <duc.nguyenminh@2359media.com>
2019-03-20 10:15:35 +0700

    Deploy

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
     serectKey: "english_camp_serectkey",
-    domain: "http://localhost:1304/",
+    domain: "http://englishcamp/hsvuit.org/",
     appId: "1057102251148374",
     appSerect: "712c9ab9806dbd2ab1989dfe0ea984fc"
 }
