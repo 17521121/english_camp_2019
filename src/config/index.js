f1230fc3656103015f0ef5dfbdebac8805109b6b
Michael John <duc.nguyenminh@2359media.com>
2019-03-22 14:54:05 +0700

    Fix bug

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ module.exports = {
     serectKey: "english_camp_serectkey",
     domain: "https://englishcamp.hsvuit.org",
     appId: "1057102251148374",
-    appSerect: "712c9ab9806dbd2ab1989dfe0ea984fc"
+    appSerect: "712c9ab9806dbd2ab1989dfe0ea984fc",
     IS_ADMIN: ["ADMIN"],
     IS_USER: ["USER"],
 }
